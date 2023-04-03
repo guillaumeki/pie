86d59f4b4c32a20a4afe3ccfa1cf634b6a5147c6
Guillaume Kihli <guillaume.kihli@yahoo.fr>
2023-04-03 22:31:03 +0200

    Disjunctive rewriting

--- parser/dlgp/test/test_dlgp2_parser.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from parser.dlgp.dlgp2_parser import Dlgp2Parser
-
-
-class TestDlgp2Parser(TestCase):
-    def test_parse_atoms(self):
-        atoms_str = "p(a), relatedTo(a,b), q(b). [f2] p(X), t(X,a,b), s(a,z), p(a)."
-        #print(Dlgp2Parser.instance().parse_atoms(atoms_str))
