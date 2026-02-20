c8f24f6bed87f956d0faf452442a8daf24502190
Guillaume Kihli <guillaume.kihli@yahoo.fr>
2026-02-20 17:58:02 +0100

    Fix unused renamer imports in chase builder

--- prototyping_inference_engine/forward_chaining/chase/chase_builder.py
+++ prototyping_inference_engine/forward_chaining/chase/chase_builder.py
@@ -60,18 +60,15 @@ from prototyping_inference_engine.forward_chaining.chase.rule_applier.trigger_ap
 )
 from prototyping_inference_engine.forward_chaining.chase.rule_applier.trigger_applier.renamer.body_skolem import (
     BodyPseudoSkolem,
-    BodySkolem,
 )
 from prototyping_inference_engine.forward_chaining.chase.rule_applier.trigger_applier.renamer.fresh_renamer import (
     FreshRenamer,
 )
 from prototyping_inference_engine.forward_chaining.chase.rule_applier.trigger_applier.renamer.frontier_by_piece_skolem import (
     FrontierByPiecePseudoSkolem,
-    FrontierByPieceSkolem,
 )
 from prototyping_inference_engine.forward_chaining.chase.rule_applier.trigger_applier.renamer.frontier_skolem import (
     FrontierPseudoSkolem,
-    FrontierSkolem,
 )
 from prototyping_inference_engine.forward_chaining.chase.rule_applier.trigger_applier.renamer.frontier_by_piece_true_skolem import (
     FrontierByPieceTrueSkolem,
